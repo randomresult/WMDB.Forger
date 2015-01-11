237b18993bf23e6530e2b654f4be1c81f63d8ed3
Mathias Schreiber <mathias.schreiber@wmdb.de>
2015-01-11 11:02:48 +0100

    removed subproject from management link to forge

--- Classes/WMDB/Forger/ViewHelpers/ForgelinkViewHelper.php
+++ Classes/WMDB/Forger/ViewHelpers/ForgelinkViewHelper.php
@@ -17,7 +17,7 @@ class ForgelinkViewHelper extends AbstractViewHelper {
 	 */
 	public function render($linkText = '', $year = '2015', $month = '01') {
 		$end = $this->getLastDayOfMonth($year, $month);
-		$content = '<a href="https://forge.typo3.org/projects/typo3cms-core/issues?set_filter=1&f[]=status_id&op[status_id]=o&f[]=updated_on&op[updated_on]=%3E%3C&v[updated_on][]='.$year.'-'.$month.'-01&v[updated_on][]='.$end.'&f[]=&c[]=tracker&c[]=status&c[]=priority&c[]=subject&c[]=assigned_to&c[]=category&c[]=fixed_version&group_by=" target="_blank">';
+		$content = '<a href="https://forge.typo3.org/projects/typo3cms-core/issues?set_filter=1&f[]=status_id&op[status_id]=o&f[]=updated_on&op[updated_on]=%3E%3C&v[updated_on][]='.$year.'-'.$month.'-01&v[updated_on][]='.$end.'&&f[]=subproject_id&op[subproject_id]=!*&f[]=&c[]=tracker&c[]=status&c[]=priority&c[]=subject&c[]=assigned_to&c[]=category&c[]=fixed_version&group_by=" target="_blank">';
 		$content .= $linkText;
 		$content .= '</a>';
 		return $content;
